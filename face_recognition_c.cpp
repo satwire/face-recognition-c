686f99c3455e1d2e207f9c4fc3cf78ec08fa250a
Satwiko Wirawan Indrawanto <satwikowirawan@live.com>
2020-10-23 18:24:49 +0700

    add opencv library

--- face_recognition_c.cpp
+++ face_recognition_c.cpp
@@ -2,20 +2,29 @@
 //
 
 #include <iostream>
+#include "opencv2/core.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
 #include "tensorflow/c/c_api.h"
+
+using namespace std;
+using namespace cv;
 
 int main()
 {
     printf("Hello from TensorFlow C library version %s\n", TF_Version());
+
+    Mat image;
+    image = imread("opencv-logo-small.png", IMREAD_COLOR);
+
+    if (image.empty()) // Check for invalid input
+    {
+        cout << "Could not open or find the image" << std::endl;
+        return -1;
+    }
+
+    namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
+    imshow("Display window", image); // Show our image inside it.
+    waitKey(0); // Wait for a keystroke in the window
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
