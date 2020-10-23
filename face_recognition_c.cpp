c50bcc7c3403a36254e401cbf8767f33cef5c146
Satwiko Wirawan Indrawanto <satwikowirawan@live.com>
2020-10-23 21:47:36 +0700

    add blob creation for neural network

--- face_recognition_c.cpp
+++ face_recognition_c.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include "opencv2/core.hpp"
 #include "opencv2/imgcodecs.hpp"
+#include "opencv2/imgproc.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/dnn.hpp"
 #include "tensorflow/c/c_api.h"
@@ -17,15 +18,21 @@ int main()
     printf("Hello from TensorFlow C library version %s\n", TF_Version());
 
     Net net = readNetFromCaffe("deploy.prototxt.txt", "res10_300x300_ssd_iter_140000.caffemodel");
-
-    Mat image;
-    image = imread("opencv-logo-small.png", IMREAD_COLOR);
+    Mat image = imread("404902_2745393247130_736455659_n.jpg", IMREAD_COLOR);
 
     if (image.empty()) // Check for invalid input
     {
         cout << "Could not open or find the image" << std::endl;
         return -1;
     }
+
+    int imageHeigt = image.rows;
+    int imageWidth = image.cols;
+
+    Mat imageResized;
+    resize(image, imageResized, Size(300, 300));
+
+    Mat blob = blobFromImage(imageResized, 1.0, Size(300, 300), Scalar(104.0, 177.0, 123.0), true);
 
     namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
     imshow("Display window", image); // Show our image inside it.
